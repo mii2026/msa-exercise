f5f0dd78eedd8bd105538dcf6cc6bc57ed9eafd1
mii2026 <mii2026@skku.edu>
2024-08-09 16:05:23 +0900

    [feat] API 게이트웨이 로그인 검증 기능 추가

---
+++ gateway/src/main/java/com/sparta/msa_exam/gateway/AuthFilter.java
@@ -0,0 +1,57 @@
+package com.sparta.msa_exam.gateway;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+
+@Configuration
+public class AuthFilter implements GlobalFilter {
+
+    @Value("${service.jwt.secret-key}")
+    private String secretKey;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+        if(path.equals("/auth/signIn")) {
+            return chain.filter(exchange);
+        }
+
+        String token = extractToken(exchange);
+        if(token == null || !validateToken(token)) {
+            exchange.getResponse().getHeaders().add("Server-Port", "19091");
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    public String extractToken(ServerWebExchange exchange){
+        String token = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if(token != null && token.startsWith("Bearer ")){
+            return token.substring(7);
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
+            Jwts.parser().verifyWith(key)
+                    .build().parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
