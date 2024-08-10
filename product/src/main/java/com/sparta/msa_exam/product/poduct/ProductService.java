2d7c7ffcc9bc051389e87bd879cb6a0d01844120
mii2026 <mii2026@skku.edu>
2024-08-11 03:46:07 +0900

    [feat] 상품 id로 조회 기능 추가

--- product/src/main/java/com/sparta/msa_exam/product/poduct/ProductService.java
+++ product/src/main/java/com/sparta/msa_exam/product/poduct/ProductService.java
@@ -3,7 +3,9 @@ package com.sparta.msa_exam.product.poduct;
 import com.sparta.msa_exam.product.core.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,5 +30,11 @@ public class ProductService {
                 .stream()
                 .map(ProductResponseDto::new)
                 .toList();
+    }
+
+    public ProductResponseDto getProduct(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found or has been deleted"));
+        return new ProductResponseDto(product);
     }
 }
