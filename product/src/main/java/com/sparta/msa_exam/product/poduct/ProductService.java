cc7bfcd7d452dcfc2926a74846fb689fd137840a
mii2026 <mii2026@skku.edu>
2024-08-11 04:32:32 +0900

    [feat] 상품 추가, 조회 기능 캐싱 구현

--- product/src/main/java/com/sparta/msa_exam/product/poduct/ProductService.java
+++ product/src/main/java/com/sparta/msa_exam/product/poduct/ProductService.java
@@ -2,13 +2,15 @@ package com.sparta.msa_exam.product.poduct;
 
 import com.sparta.msa_exam.product.core.Product;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,15 +18,18 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public Product createProduct(ProductRequestDto requestDto) {
+    @CachePut(cacheNames = "productCache", key = "#result.product_id")
+    @CacheEvict(cacheNames = "productAllCache", allEntries = true)
+    public ProductResponseDto createProduct(ProductRequestDto requestDto) {
         Product product = Product.builder()
                 .name(requestDto.getName())
                 .supplyPrice(requestDto.getSupply_price())
                 .build();
 
-        return productRepository.save(product);
+        return new ProductResponseDto(productRepository.save(product));
     }
 
+    @Cacheable(cacheNames = "productAllCache")
     public List<ProductResponseDto> getProducts(Pageable pageable) {
         return productRepository.findAll(pageable)
                 .stream()
@@ -32,6 +37,7 @@ public class ProductService {
                 .toList();
     }
 
+    @Cacheable(cacheNames = "productCache", key = "args[0]")
     public ProductResponseDto getProduct(Long id) {
         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found or has been deleted"));
