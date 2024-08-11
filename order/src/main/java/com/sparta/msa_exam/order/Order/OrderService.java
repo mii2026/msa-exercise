19abbd4dac6d9c419f68d2449b4e41f91c7e4868
mii2026 <mii2026@skku.edu>
2024-08-12 01:27:08 +0900

    [feat] 주문 상품 추가 기능 구현 다시 업로드

--- order/src/main/java/com/sparta/msa_exam/order/Order/OrderService.java
+++ order/src/main/java/com/sparta/msa_exam/order/Order/OrderService.java
@@ -5,11 +5,15 @@ import com.sparta.msa_exam.order.core.client.ProductResponseDto;
 import com.sparta.msa_exam.order.core.domain.Order;
 import com.sparta.msa_exam.order.core.domain.OrderProduct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,27 +25,57 @@ public class OrderService {
 
     @Transactional
     public OrderResponseDto createOrder(OrderRequestDto requestDto) {
-        for (Long productId : requestDto.getProduct_ids()) {
-            ProductResponseDto product = productClient.getProduct(productId);
+        Set<Long> productIds = productClient.getProduct().stream()
+                .map(ProductResponseDto::getProduct_id)
+                .collect(Collectors.toSet());
+        for (Long productId: requestDto.getProduct_ids()) {
+            if(!productIds.contains(productId)){
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Product not found");
+            }
         }
 
         Order order = Order.builder()
                 .name(requestDto.getName())
                 .build();
+        order = orderRepository.save(order);
 
-        List<OrderProduct> products = new ArrayList<>();
+        List<OrderProduct> orderProducts = new ArrayList<>();
         for(Long i: requestDto.getProduct_ids()){
             OrderProduct orderProduct = OrderProduct.builder()
                     .productId(i)
                     .order(order)
                     .build();
-            products.add(orderProduct);
+            orderProducts.add(orderProduct);
         }
+        orderProductRepository.saveAll(orderProducts);
 
-        order = orderRepository.save(order);
-        orderProductRepository.saveAll(products);
-        order.setProductIds(products);
+        order.setProductIds(orderProducts);
+
         return new OrderResponseDto(order);
     }
 
+    @Transactional
+    public OrderResponseDto updateOrder(Long orderId, Long productId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Order not found"));
+
+        Set<Long> productIds = productClient.getProduct().stream()
+                .map(ProductResponseDto::getProduct_id)
+                .collect(Collectors.toSet());
+        if(!productIds.contains(productId)){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Product not found");
+        }
+
+        OrderProduct orderProduct = OrderProduct.builder()
+                        .order(order)
+                        .productId(productId)
+                        .build();
+        orderProductRepository.save(orderProduct);
+
+        List<OrderProduct> orderProducts = order.getProductIds();
+        orderProducts.add(orderProduct);
+        order.setProductIds(orderProducts);
+
+        return new OrderResponseDto(order);
+    }
 }
--- order/src/main/java/com/sparta/msa_exam/order/Order/UpdateOderDto.java
+++ order/src/main/java/com/sparta/msa_exam/order/Order/UpdateOderDto.java
@@ -1,2 +1,8 @@
-package com.sparta.msa_exam.order.Order;public class UpdateOderDto {
+package com.sparta.msa_exam.order.Order;
+
+import lombok.Data;
+
+@Data
+public class UpdateOderDto {
+    private Long product_id;
 }
